5b9d23dd43bb006d1ee6d3a1a813aefbf2c368b3
Ali Siddiqi <alisiddiqui275@gmail.com>
2025-10-30 20:32:59 +0500

    Updating train and ingestion and adding requirements

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -38,6 +38,10 @@ def load_csv(path: str) -> pd.DataFrame:
 
 def drop_na(df: pd.DataFrame) -> pd.DataFrame:
     """Drop rows with NA values (same as notebook)."""
+    target_col = 'price_doc'  # Replace with actual target column name
+    # Keep first 4 columns plus target
+    cols_to_keep = df.columns[:4].tolist() + [target_col]
+    df = df[cols_to_keep]
     return df.dropna(axis=0)
 
 
--- src/train.py
+++ src/train.py
@@ -132,9 +132,9 @@ def main():
     logger.info("Training run finished. MLflow run info available.")
 
     # DATA DRIFT evidently
-    train_path = r"D:\Ikhlas University\Semester 7\MLOPS\Project_Financial_Advisor\MLOps--Finance-Assistant\train.csv"
-    test_path = r"D:\Ikhlas University\Semester 7\MLOPS\Project_Financial_Advisor\MLOps--Finance-Assistant\test.csv"
-    generate_data_drift_report(train_path, test_path, "data_drift_report.html")
+    #train_path = r"D:\Ikhlas University\Semester 7\MLOPS\Project_Financial_Advisor\MLOps--Finance-Assistant\train.csv"
+    #test_path = r"D:\Ikhlas University\Semester 7\MLOPS\Project_Financial_Advisor\MLOps--Finance-Assistant\test.csv"
+    #generate_data_drift_report(train_path, test_path, "data_drift_report.html")
 
 
 if __name__ == "__main__":
