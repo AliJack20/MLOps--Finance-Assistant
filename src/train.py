b960ca9fc024c1f3a5aa66a03a62fe9ee3028809
Ali Siddiqi <alisiddiqui275@gmail.com>
2025-10-30 17:34:29 +0500

    fixed ruff errors

--- src/train.py
+++ src/train.py
@@ -7,7 +7,8 @@ Train script:
 - Uploads a copy of the fitted model as models/latest_model.pkl to S3 (S3_BUCKET env)
 """
 
-import os, sys
+import os
+import sys
 import tempfile
 import joblib
 import mlflow
@@ -122,8 +123,8 @@ def main():
             local_model_path = tf.name
 
         # Upload to S3 (explicit)
-        # upload_file_to_s3(local_model_path, S3_BUCKET, S3_MODEL_KEY)
-        # logger.info("Model uploaded to s3://%s/%s", S3_BUCKET, S3_MODEL_KEY)
+        upload_file_to_s3(local_model_path, S3_BUCKET, S3_MODEL_KEY)
+        logger.info("Model uploaded to s3://%s/%s", S3_BUCKET, S3_MODEL_KEY)
 
         # Log S3 location as tag/artifact
         mlflow.set_tag("s3_model_path", f"s3://{S3_BUCKET}/{S3_MODEL_KEY}")
